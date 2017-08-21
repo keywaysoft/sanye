9869644a8d2bb3bd5fa7b79be6a3601040fd2b1d
sjoooo123 <sujun198733@126.com>
2017-08-21 22:18:22 +0800

    政务服务1

--- mobile/js/base.js
+++ mobile/js/base.js
@@ -16,6 +16,19 @@ var base = {
 				}
 			}
 		});
+	},
+	hasClass: function( elements,cName ){ 
+		return !!elements.className.match( new RegExp( "(\\s|^)" + cName + "(\\s|$)") );  
+	},
+	addClass: function( elements,cName ){ 
+		if( !base.hasClass( elements,cName ) ){ 
+			elements.className += " " + cName; 
+		}; 
+	},
+	removeClass: function( elements,cName ){ 
+		if( base.hasClass( elements,cName ) ){ 
+			elements.className = elements.className.replace( new RegExp( "(\\s|^)" + cName + "(\\s|$)" )," " );  
+		}; 
 	}
 }
 
--- mobile/js/page/fw.js
+++ mobile/js/page/fw.js
@@ -1,16 +1,24 @@
 mui.init();
 (function($) {
 	//滚动
-	mui('.mui-scroll-wrapper').scroll({
+	$('.mui-scroll-wrapper').scroll({
 		deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 	});
 	
-	//选项卡-1
-	var htmlArr = [],
-		itemArr = [];
-	htmlArr[0] = '<ul class="mui-table-view"><li class="mui-table-view-cell"><a href="javascript:;"><p class="mui-ellipsis">安州区花荄镇、桑枣镇将成为南开大学基层实践基地</p><span class="time">2017-08-15</span></a></li><li class="mui-table-view-cell"><a href="javascript:;"><p class="mui-ellipsis">廖雪梅率队督导安州区环境保护突出问题</p><span class="time">2017-08-15</span></a></li><li class="mui-table-view-cell"><a href="javascript:;"><p class="mui-ellipsis">安州召开集贸市场创文工作现场推进会</p><span class="time">2017-08-15</span></a></li><li class="mui-table-view-cell"><a href="javascript:;"><p class="mui-ellipsis">四川日报：安州“创文”小举动折射大文明</p><span class="time">2017-08-15</span></a></li><li class="mui-table-view-cell"><a href="javascript:;"><p class="mui-ellipsis">安州城西将建连江公园造价3.5亿元绿地率82.5%</p><span class="time">2017-08-15</span></a></li><li class="mui-table-view-cell"><a href="javascript:;"><p class="mui-ellipsis">安州区发放首批四川省工会会员服务卡</p><span class="time">2017-08-15</span></a></li></ul>';
-	itemArr[0] = document.getElementById('item1');
-	itemArr[1] = document.getElementById('item2')
-	base.slideFun(htmlArr,itemArr,'slider');
+	//选项卡
+	$(".h-title-tab").on("tap","span:not(.active)",function(){
+		base.removeClass(this.parentNode.querySelector(".active"),"active");
+		base.addClass(this,"active");
+		//切换
+		base.removeClass(document.querySelector(this.getAttribute("data-target")).parentNode.querySelector(".active"),"active");
+		base.addClass(document.querySelector(this.getAttribute("data-target")),"active");
+	})
+	//更多
+	$(".tab-con").on("tap",".more",function(){
+		$(".tab-con .active li").each(function(i,e){
+			e.style.display = "block";
+		})
+		this.style.display = "none";
+	})
 	
 })(mui);
